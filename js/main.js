75a917bdae4fe623916d9fe87bef39a1bc73bd03
16096320 <31434565+16096320@users.noreply.github.com>
2020-05-20 16:02:51 +0200

    video toegevoegd

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+function vidPlay(video) {
+	var element = document.getElementById(video);
+	element.play();
+}
+
+function vidPause(video) {
+	var element = document.getElementById(video);
+	element.pause();
+}
