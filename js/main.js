f6056c2a6c949a5b66a6b5a72a951f1e3dac95a1
16096320 <31434565+16096320@users.noreply.github.com>
2020-05-20 16:07:08 +0200

    consolelog for functions

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@
 function vidPlay(video) {
 	var element = document.getElementById(video);
 	element.play();
+	console.log("video plays");
 }
 
 function vidPause(video) {
 	var element = document.getElementById(video);
 	element.pause();
+	console.log("video pauses");
 }
